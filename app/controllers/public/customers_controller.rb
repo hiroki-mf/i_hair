390c09432e6c546ba46336c8eb1298c79a7b404d
EC2 Default User <ec2-user@ip-172-31-30-169.ap-northeast-1.compute.internal>
2023-04-11 11:26:50 +0000

    kaiinnkinou

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,33 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+   if  @customer.update(customer_params)
+    redirect_to mypage_customer_path
+   else
+    render :edit
+   end
+  end
+  
+  def unsubscribe
+  end
+  
+  def withdrawal
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+  
+   private
+  def customer_params
+    params.require(:customer).permit(:name, :email, :birthday, :sex)
+  end
 end
