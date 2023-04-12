76b305174938e03873d071c8b8aa03790566bdb4
EC2 Default User <ec2-user@ip-172-31-30-169.ap-northeast-1.compute.internal>
2023-04-12 11:46:26 +0000

    会員の修正とコスメ・レビューの作成中

--- app/controllers/public/cosmetics_controller.rb
+++ app/controllers/public/cosmetics_controller.rb
@@ -1,2 +1,11 @@
 class Public::CosmeticsController < ApplicationController
+  def index
+    @cosmetics = Cosmetic.all
+  end
+
+  
+  private
+  def item_params
+    params.require(:cosmetics).permit(:image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,13 +20,13 @@ devise_for :customers,skip: [:passwords], controllers: {
 scope module: :public do
     root to: "homes#top"
     get '/about' => 'homes#about'
-    resources :cosmetics, only: [:index, :new]
+    resources :cosmetics, only: [:index]
     get 'customers/unsubscribe', to: 'customers#unsubscribe', as: 'unsubscribe_customer'
     patch 'customers/withdrawal', to: 'customers#withdrawal', as: 'withdrawal_customer'
     get 'customers/mypage', to: 'customers#show', as: 'mypage_customer'
     get 'customers/information/edit', to: 'customers#edit', as: 'information_edit_customer'
     patch 'customers/information', to: 'customers#update', as: 'information_customer'
-    resources :reviews, only: [:index, :show]
+    resources :reviews, only: [:index, :show, :new]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
